447a5c2df494f74f06b9979fd3ae6f179e40db3d
joshasgard <idowuodesanmi@gmail.com>
2022-01-07 15:01:23 +0100

    DEPLOYMENT: UPDATE APP FOR DEPLOYMENT
    Requirements.txt, .gitignore and a Procfile were all added to the project for Heroku deployment. settings.py is then updated with dependencies

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
 from pathlib import Path
+import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--js(r^6j$e#=#$$)ebccd(nhgz_u^$#5^l8dd6dm3dbfq_6l_h'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://free-password-generator.herokuapp.com','localhost', '127.0.0.1']
 
 
 # Application definition
@@ -118,6 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
